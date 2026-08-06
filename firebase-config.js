eafd83df219d5d23a92363943334fbe73a0f2aad
Daylamiy06 <108115912+Daylamiy06@users.noreply.github.com>
2026-08-06 17:46:30 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5mzmSOCCfgtroci2z330Dmk-CzPGVOqk",
+  authDomain: "cceeh-db.firebaseapp.com",
+  databaseURL: "https://cceeh-db-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "cceeh-db",
+  storageBucket: "cceeh-db.firebasestorage.app",
+  messagingSenderId: "850138912748",
+  appId: "1:850138912748:web:9c74b38773eff5e5f592c9",
+  measurementId: "G-SY10ZKLTDS"
+};
+
+export { firebaseConfig };
